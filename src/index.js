62b8b608bb70327d61f9c637428cd00e3c4d875a
Dharanish <dharanishpm@gmail.com>
2022-07-19 22:25:30 +0530

    jsx jsx rules and nested components dev tools

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react'
-import  ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 
+// stateless functional component
+// always return JSX
+
+
+// JSX Rules
+// return single element
+// div/section/article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
+
+//Nested coponents,reat tools
 function Greeting(){
-  return <h2></h2>
+  return (
+    <React.Fragment>
+      <div>
+        <Person />
+        <Message />
+        
+        <h2>helllo</h2>
+        <img src="" alt="" />
+      </div>
+
+    </React.Fragment>
+
+  );
 }
+
+const Person = () => {
+  return (<h2>First message</h2>)
+}
+const Message = ()=> <h1>Secind Message</h1>
 
 ReactDOM.render(<Greeting/>,document.getElementById('root'));
