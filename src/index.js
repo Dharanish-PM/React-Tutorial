ae2c9aa6a82f5a070b979ea8cd117b012cbdac9d
Dharanish <dharanishpm@gmail.com>
2022-07-20 12:12:31 +0530

    Doenst render array of objects

--- src/index.js
+++ src/index.js
@@ -5,51 +5,52 @@ import { createRoot } from 'react-dom/client';
 //CSS
 import './index.css';
 
-//Settign up objects
 
-const firstbook = {
-  img: 'https://images-eu.ssl-images-amazon.com/images/I/51TehdI7m3L._AC_SX184_.jpg',
-  title: 'I love You to the moon and backsss',
-  Author: 'Amelia'
-  
-}
+const Books = [
+  {
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/51TehdI7m3L._AC_SX184_.jpg',
+    title: 'I love You to the moon and backsss',
+    Author: 'Amelia'
+  },
+  {
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/51TehdI7m3L._AC_SX184_.jpg',
+    title: 'I love You to the moon and backsss',
+    Author: 'Amelia'
+  },
 
+];
+
+
+/*React wont render array of obejects*/ 
+const names = ['a', 'dfb0', 'dsv'];
+
+const newNames = names.map((name) => {
+  return <h1>{ name}</h1>
+})
 
 function Booklist(){
   return (
     <section className='booklist'>
-      <Book img={firstbook.img} title={firstbook.title} Author={firstbook.Author}>
-        
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sunt in incidunt placeat quis quam necessitatibus expedita dolorum laudantium fuga?</p>
-
-      </Book>
+      {/* { Books}==>NOt possible */} 
+      {names}
+      {newNames}
       
-
-      <Book img={firstbook.img} title={firstbook.title} Author={firstbook.Author}  />
+      
     
     </section>
   );
 }
 
 
-const Book = (props) => {/*const Book = ({img, title, Author })*/
+const Book = (props) => {
 
   const { img, title, Author } = props;
-  console.log(props)/*You get the pararagraph element also in props in name of children*/ 
-  // const { img, title, Author,children } = props;
-  
- 
+  console.log(props)
   return (
     <article className='book'>
        <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{Author}</h4>
-      { props.children}
-
-      {/* <p>{ props.job}</p> */}
-      {/* <p>{ props.name}</p> */}
-      {/* <p>{props.number}</p> */}
-      {/* If it is there in the pros it gets diplyed else not */}
     </article>
   );
   
