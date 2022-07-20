1477f198108b6349964757c096d62a6e24d72fdd
Dharanish <dharanishpm@gmail.com>
2022-07-20 11:28:21 +0530

    React 18 render and usage of Props

--- src/index.js
+++ src/index.js
@@ -1,53 +1,53 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
+
 
 //CSS
 import './index.css';
 
-// stateless functional component
-// always return JSX
+//Settign up objects
+
+const firstbook = {
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/51TehdI7m3L._AC_SX184_.jpg',
+  title: 'I love You to the moon and backsss',
+  Author: 'Amelia'
+  
+}
 
 
-// JSX Rules
-// return single element
-// div/section/article or Fragment
-// use camelCase for html attribute
-// className instead of class
-// close every element
-// formatting
-
-
-//Nested coponents,reat dev tools==> person and message
 function Booklist(){
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book img={firstbook.img} title={firstbook.title} Author={firstbook.Author } />
+      <Book  />
+    
     </section>
 
   );
 }
 
-const Author='Amelia'
-const Book = () => {
-  const title='I love You to the moon and backsss'
+
+const Book = (props) => {
+  console.log(props);
+ 
   return (
     <article className='book'>
-       <img src="https://images-eu.ssl-images-amazon.com/images/I/51TehdI7m3L._AC_SX184_.jpg" alt="" srcset="" />
-      <h1>{title}</h1>
+       <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.Author}</h4>
 
-      
-      <h4>{Author.toUpperCase()}</h4>
-      {/* <h3>{ let x=5}</h3>  ==> not possible only returning stuffs can be in here not statements*/}
-      <h3>{ 6+6}</h3>
+
+
+      {/* <p>{ props.job}</p> */}
+      {/* <p>{ props.name}</p> */}
+      {/* <p>{props.number}</p> */}
+      {/* If it is there in the pros it gets diplyed else not */}
     </article>
   );
   
 };
 
 
-
-ReactDOM.render(<Booklist/>,document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Booklist/>);
